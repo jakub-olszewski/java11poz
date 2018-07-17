d3c7f6bcbdd07e611b9466304739900c568c2e9c
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-17 20:49:14 +0200

    zmienne i metody

--- src/main/java/java11poz/Main.java
+++ src/main/java/java11poz/Main.java
@@ -2,7 +2,6 @@
  * 
  */
 package java11poz;
-import static java.lang.Math.PI;
 
 /**
  * @author trener
@@ -22,8 +21,24 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello !"); //ten kod wypisuje napis Hello ! na konsole
+		System.out.println("Hello !"); // ten kod wypisuje napis Hello ! na konsole
 		System.out.println(Math.PI); // ten kod wypisuje liczbe PI na konsole
+
+		int liczba; // deklaracja zmiennej
+		liczba = 5; // inicjlizacja zmiennej
+		int liczbaOsob = 15; // deklaracja i inicjalizacja jednocześnie
+		int liczbaDrzwiWSamochodzie = 0;
+		System.out.println("liczba " + liczbaDrzwiWSamochodzie);
+		wypiszArgumentMetody(10);
+		System.out.println("5 + 3");
+		System.out.println(5 + 3);
+
+		// TODO zrob metode sumaLiczb a b c i przypisz do zmiennej
+
+	}
+
+	public static void wypiszArgumentMetody(int argument) {
+		System.out.println(argument + " to jest argument metody.");
 	}
 
 }
