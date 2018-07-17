c4d5fbf2928f22f932874e08b282ac06c1738197
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-17 20:08:01 +0200

    program hello world wypisywanie na konsole

--- src/main/java/java11poz/Main.java
+++ src/main/java/java11poz/Main.java
@@ -1,5 +1,29 @@
+/**
+ * 
+ */
 package java11poz;
+import static java.lang.Math.PI;
 
+/**
+ * @author trener
+ *
+ */
 public class Main {
+
+	/**
+	 * to jest komentarz wystepujacy na poczatku metody/konstruktora
+	 */
+	public Main() {
+		// to jest komentarz wystepujacy w bloku kodu - miedzy klamerkami
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello !"); //ten kod wypisuje napis Hello ! na konsole
+		System.out.println(Math.PI); // ten kod wypisuje liczbe PI na konsole
+	}
 
 }
