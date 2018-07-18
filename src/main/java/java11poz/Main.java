9159f5e1e8e4ac39b02f43835a96789711eba679
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-18 17:46:32 +0200

    metody

--- src/main/java/java11poz/Main.java
+++ src/main/java/java11poz/Main.java
@@ -20,7 +20,6 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello !"); // ten kod wypisuje napis Hello ! na konsole
 		System.out.println(Math.PI); // ten kod wypisuje liczbe PI na konsole
 
@@ -33,12 +32,28 @@ public class Main {
 		System.out.println("5 + 3");
 		System.out.println(5 + 3);
 
+		int a = 5;
+		int b = 6;
+		int c = 1;
+
+		System.out.println("suma trzech liczb wynosi " + sumaLiczb(a, b, c));
+		System.out.println("suma trzech liczb wynosi " + sumaLiczb(sumaLiczb(a, b), c));
 		// TODO zrob metode sumaLiczb a b c i przypisz do zmiennej
+		// dopisac komentarze
 
 	}
 
+	// return wystepuje gdy mamy metodę ktora zwraca np int
+	// w przypadku void nie powinno byc return
 	public static void wypiszArgumentMetody(int argument) {
 		System.out.println(argument + " to jest argument metody.");
 	}
 
+	public static int sumaLiczb(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static int sumaLiczb(int a, int b) {
+		return a + b;
+	}
 }
