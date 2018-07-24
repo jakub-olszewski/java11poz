01caa7feab7f97500350f857f9888783c9eb1b96
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-24 20:11:50 +0200

    cwiczenie petla while

---
+++ src/main/java/pl/sdacademy/java11poz/main/PetlaWhile.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.java11poz.main;
+
+import java.util.logging.Logger;
+
+public class PetlaWhile {
+
+	// utworzenie statycznego loggera
+	protected final static Logger logger = Logger.getLogger(PetlaFor.class.getName()); // java.util.logging.Logger
+
+	public static void main(String[] args) {
+		// TODO utworz tablice liczb::float 6 elementow
+
+		float[] tablicaLiczb = { 99f, 45f, 101f, 78f, 32f, 6f };
+
+		// TODO wypisz elementy wykorzystujac petle while
+		// warunek liczba < 100
+	}
+
+}
