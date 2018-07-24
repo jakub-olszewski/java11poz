3926072f54483d4d205eadfa984429c4ca3166ae
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-24 20:40:08 +0200

    zadanie 1 petle

---
+++ src/main/java/pl/sdacademy/java11poz/main/Zadanie1Petle.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java11poz.main;
+
+/**
+ * Wypisz parzyste elementy utworzonej wcześniej tablicy zawierającą liczby</br>
+ * Wypisz elementy utworzonej wcześniej tablicy zawierającą imiona na literę ‚a’
+ * 
+ * @author trener
+ *
+ */
+public class Zadanie1Petle {
+
+	String[] imiona = { "Julia", "Jarek", "Mateusz" };// TODO dopisz imiona
+
+	// uzywajac petli wypisz parzyste pozycje indeksu
+
+	/**
+	 * wypisz imiona zawierajace litere 'a'a
+	 */
+
+}
