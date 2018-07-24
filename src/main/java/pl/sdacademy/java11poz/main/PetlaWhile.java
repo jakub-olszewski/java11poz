b10d09bbe92bced1f25872b58115e3f8fd9b8157
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-24 20:35:36 +0200

    cwiczenie petla while

--- src/main/java/pl/sdacademy/java11poz/main/PetlaWhile.java
+++ src/main/java/pl/sdacademy/java11poz/main/PetlaWhile.java
@@ -1,5 +1,6 @@
 package pl.sdacademy.java11poz.main;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PetlaWhile {
@@ -14,6 +15,28 @@ public class PetlaWhile {
 
 		// TODO wypisz elementy wykorzystujac petle while
 		// warunek liczba < 100
+
+		int index = 0;// stan poczatkowy indeksu tablicy od 0 do długości tablicy czyli 6
+		float liczba = tablicaLiczb[index];
+
+		boolean warunek = true;// liczba mniejsza niz 100
+		while (warunek) {
+
+			if (tablicaLiczb[index] < 100 || index > tablicaLiczb.length - 1) {
+				logger.log(Level.INFO, tablicaLiczb[index] + "");
+
+			}
+			else {
+				warunek = false;
+			}
+			index++;
+
+			// if (index > tablicaLiczb.length - 1) {
+			// warunek = false;
+			// }
+
+			// TODO zwieksz indeks
+		}
 	}
 
 }
