cfce67842dfaaf2b89a675973dc5007f5b6995c1
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-24 20:05:10 +0200

    cwiczenie petlaFor 2

---
+++ src/main/java/pl/sdacademy/java11poz/main/PetlaFor2.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.java11poz.main;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PetlaFor2 {
+
+	// utworzenie statycznego loggera
+	protected final static Logger logger = Logger.getLogger(PetlaFor2.class.getName()); // java.util.logging.Logger
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] pozycje = { "pizza1", "napoj8", "makaron3" };
+
+		for (String pozycja : pozycje) {
+			logger.log(Level.INFO, "Przetwarzam element: " + pozycja);
+
+		}
+
+	}
+
+}
