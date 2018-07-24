ba1637a4bc33a779b1d055efc521c1e603d00a81
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-24 19:56:43 +0200

    cwiczenia petla for

--- src/main/java/pl/sdacademy/java11poz/main/PetlaFor.java
+++ src/main/java/pl/sdacademy/java11poz/main/PetlaFor.java
@@ -1,16 +1,39 @@
 package pl.sdacademy.java11poz.main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Program do testowania petli for
  * @author trener
  *
  */
 public class PetlaFor {
+	// utworzenie statycznego loggera
+	protected final static Logger logger = Logger.getLogger(PetlaFor.class.getName()); // java.util.logging.Logger
 
 	public static void main(String[] args) {
-		// TODO utworz tablice liczb 6 elementow
+		// TODO utworz tablice liczb::float 6 elementow
 
+		float[] tablicaLiczb = { 99f, 45f, 101f, 78f, 32f, 6f };
+
+		for (float x : tablicaLiczb) {
+			logger.log(Level.INFO, "Przetwarzam element: " + x);
+			if (x > 100) {
+				break; // przerwanie pętli
+			}
+		}
+		// petla for z wykorzystaniem zmiennej pomocnicznej "i"
+		// i skrot od słowa index
+		// for (inicjalizacja zmiennej pomocniczej; warunek kontynuacji; skok/kropka)
+		// i++ -> i + 1
+		for (int i = 0; i < tablicaLiczb.length; i++) {
+			float f = tablicaLiczb[i];
+
+		}
 		// TODO uzywajac petli for wypisz elementy tablicy
+		logger.log(Level.INFO, "Przetwarzam {} elementów  " + tablicaLiczb.length);
+
 	}
 
 }
