b6ce6640bba74ccb3ddfa4086f15b708a4584dcd
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-26 21:01:13 +0200

    cwiczenie scanner system in

--- src/main/java/pl/sdacademy/java11poz/scanner/ScannerSystemIn.java
+++ src/main/java/pl/sdacademy/java11poz/scanner/ScannerSystemIn.java
@@ -1,5 +1,18 @@
 package pl.sdacademy.java11poz.scanner;
 
+import java.util.Scanner;
+
 public class ScannerSystemIn {
 
+	public static void main(String[] args) {
+
+		// System.in wprowadzanie znakow przez system
+		Scanner skaner = new Scanner(System.in);
+
+		System.out.println("Wprowadz imie:");
+		String napis = skaner.nextLine();// wczytanie znakow koniec wprowadzania to enter
+
+		System.out.println("Witaj " + napis + " :)");
+
+	}
 }
