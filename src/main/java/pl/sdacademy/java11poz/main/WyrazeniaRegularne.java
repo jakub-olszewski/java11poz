c2cac270b9a933de76501a52369f1e2c0a67f144
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-27 21:01:00 +0200

    rozwiazanie wyrazenia regularne cwiczenie

--- src/main/java/pl/sdacademy/java11poz/main/WyrazeniaRegularne.java
+++ src/main/java/pl/sdacademy/java11poz/main/WyrazeniaRegularne.java
@@ -13,18 +13,21 @@ public class WyrazeniaRegularne {
 		System.out.println(matcher.find()); // wyszukanie zawierajacego
 		System.out.println(matcher.matches()); // wyszukanie rownego/wystepujacego
 
-		compiledPattern = Pattern.compile("\\d{3}");
-		matcher = compiledPattern.matcher("3");
-		System.out.println(matcher.matches());
+		compiledPattern = Pattern.compile("\\d{3}-\\d{3}-\\d{3}");
+		matcher = compiledPattern.matcher("387-123-123");
+		System.out.println("Czy numer jest ok ? " + matcher.matches());
 
 		boolean poprawnyNumer = czyNumerTelefonuJestPoprawny("123-123-123");
 		System.out.println("Numer jest " + poprawnyNumer);
 
 		// dla chętnych wprowadzenie numeru z klawiatury
+		System.out.println("ala\\tma\\nkota");
 	}
 
-	private static boolean czyNumerTelefonuJestPoprawny(String string) {
-		return false;
+	private static boolean czyNumerTelefonuJestPoprawny(String numerTelefonu) {
+		Pattern compiledPattern = Pattern.compile("\\d{3}-\\d{3}-\\d{3}");
+		Matcher matcher = compiledPattern.matcher(numerTelefonu);
+		return matcher.matches();
 	}
 
 }
