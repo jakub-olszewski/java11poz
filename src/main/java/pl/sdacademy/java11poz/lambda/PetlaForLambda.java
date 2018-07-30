9a6bd482417e993e07cffd660d10edd219d91bae
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-30 19:30:52 +0200

    petla for lambda

---
+++ src/main/java/pl/sdacademy/java11poz/lambda/PetlaForLambda.java
@@ -0,0 +1,27 @@
+package pl.sdacademy.java11poz.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.sdacademy.java11poz.figury.abstrakcja.Figura;
+import pl.sdacademy.java11poz.figury.abstrakcja.Kwadrat;
+import pl.sdacademy.java11poz.figury.abstrakcja.Prostokąt;
+import pl.sdacademy.java11poz.figury.abstrakcja.Trapez;
+
+public class PetlaForLambda {
+
+	public static void main(String[] args) {
+
+		// tworzenie figur
+		Figura kwadratFigura = new Kwadrat(3);
+		Figura prostokatFigura = new Prostokąt(3, 5);
+		Figura trapezFigura = new Trapez(3, 8, 4);
+
+		// dodawnie do listy figury
+		List<Figura> figury = new ArrayList<>();
+		figury.add(kwadratFigura);
+		figury.add(prostokatFigura);
+		figury.add(trapezFigura);
+
+	}
+}
---
+++ src/main/java/pl/sdacademy/java11poz/main/Test.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java11poz.main;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		pobierzLiczbeSekund(-1);
+	}
+
+	public static int pobierzLiczbeSekund(int godzina) {
+		if (godzina < 0) {
+			throw new IllegalArgumentException("Godzina musi byc >= 0: " + godzina);
+		}
+		return godzina * 60 * 60;
+	}
+}
