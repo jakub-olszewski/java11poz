7d0441f82028815f35181a3acf25ba0a2c2a524c
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-07-31 18:35:51 +0200

    rozwiazanie wyjatki cwiczenie 1

--- src/main/java/pl/sdacademy/java11poz/wyjatki/WyjatkiCwiczenie1.java
+++ src/main/java/pl/sdacademy/java11poz/wyjatki/WyjatkiCwiczenie1.java
@@ -1,5 +1,7 @@
 package pl.sdacademy.java11poz.wyjatki;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class WyjatkiCwiczenie1 {
@@ -8,9 +10,42 @@ public class WyjatkiCwiczenie1 {
 
 		Date data = pobierzDate(1, 6, 2018);
 		System.out.println("Data: " + data);
+		// try {
+		// // FormatDaty.wypiszDateWFormacie("", "dd/mm/yyyy");
+		// }
+		// catch (ParseException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		System.out.println("Nic sie nie stało wyjątek obsłużony :)");
 	}
 
 	public static Date pobierzDate(int dzien, int miesiac, int rok) {
-		return new Date();
+
+		String napisData = dzien + "/" + miesiac + "/" + rok;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataPrzeksztalconaZNapisu = null;
+		try {
+			dataPrzeksztalconaZNapisu = dateFormat.parse(napisData);
+		}
+		catch (ParseException e) {
+			System.err.println("Nie prawidłowy format daty!");
+		}
+
+		if (dzien < 0) {
+			throw new IllegalArgumentException("Dzien musi byc >= 0: " + dzien);
+		}
+		if (miesiac < 0) {
+			throw new IllegalArgumentException("Miesiac musi byc >= 0: " + miesiac);
+		}
+		if (rok < 0) {
+			throw new IllegalArgumentException("Rok musi byc >= 0: " + rok);
+		}
+		// zrzucic wyjatki w przypadku nie prawidłowych
+		// danych wejściowych dzien miesiac rok
+
+		// w przypadku prawidłowych danych wejściowych
+		// tworzymy datę
+		return dataPrzeksztalconaZNapisu;
 	}
 }
