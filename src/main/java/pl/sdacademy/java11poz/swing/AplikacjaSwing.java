1a19dc5d86394c207a71adf81c2fc259e40a9f84
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-08-03 17:54:15 +0200

    zadanie swing componenty

--- src/main/java/pl/sdacademy/java11poz/swing/AplikacjaSwing.java
+++ src/main/java/pl/sdacademy/java11poz/swing/AplikacjaSwing.java
@@ -1,14 +1,39 @@
 package pl.sdacademy.java11poz.swing;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class AplikacjaSwing {
+
+	protected final static Logger logger = Logger
+			.getLogger(AplikacjaSwing.class.getName()); // java.util.logging.Logger
 
 	public static void main(String[] args) {
 		String title = "Moja pierwsza aplikacja";
 		// okienko
 		JFrame frame = new JFrame(title);
-		frame.setVisible(true);
+
+		// przycisk
+		String label = "Click me!";
+		JButton button = new JButton(label);
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				logger.info("Click button");
+			}
+		});
+		// button.setBounds(100, 100, 100, 40);
+		frame.add(button);
+
+		frame.setVisible(true);// ustawienie widoczności okienka
+		frame.setSize(480, 600);// ustawienie rozmiaru okienka
+		frame.setLayout(new GridLayout(3, 1));
 	}
 
 }
