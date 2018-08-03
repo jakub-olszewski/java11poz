5edcc0ae4fc4d0176f71d98da9e6d76a1a296f5a
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-08-03 18:20:46 +0200

    zadanie lista pozycji uzywajac JList

--- src/main/java/pl/sdacademy/java11poz/swing/AplikacjaSwing.java
+++ src/main/java/pl/sdacademy/java11poz/swing/AplikacjaSwing.java
@@ -1,12 +1,20 @@
 package pl.sdacademy.java11poz.swing;
 
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
 import java.util.logging.Logger;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextField;
+
+import pl.sdacademy.java11poz.jadalnia.MapaPozycjiZamowienia;
+import pl.sdacademy.java11poz.jadalnia.PozycjaZamowienia;
 
 public class AplikacjaSwing {
 
@@ -31,9 +39,42 @@ public class AplikacjaSwing {
 		// button.setBounds(100, 100, 100, 40);
 		frame.add(button);
 
+		DefaultListModel<String> listModel = new DefaultListModel<>();
+
+		// petli / stream / for zrób listę z mapy pozycji
+		MapaPozycjiZamowienia mapaPozycji = new MapaPozycjiZamowienia();
+		Collection<PozycjaZamowienia> kolekcjaPozycji = mapaPozycji.getPozycjeMapa()
+				.values();
+		listModel.addElement("USA");
+		listModel.addElement("India");
+		listModel.addElement("Vietnam");
+		listModel.addElement("Canada");
+		listModel.addElement("Denmark");
+		listModel.addElement("France");
+		listModel.addElement("Great Britain");
+		listModel.addElement("Japan");
+
+		// create the list
+		JList countryList = new JList<>(listModel);
+		frame.add(countryList);
+
+		JTextField textField = new JTextField("This is a text", 20);
+		frame.add(textField);
+
+		countryList.setBackground(Color.green);
 		frame.setVisible(true);// ustawienie widoczności okienka
 		frame.setSize(480, 600);// ustawienie rozmiaru okienka
-		frame.setLayout(new GridLayout(3, 1));
+		// frame.setLayout(new GridLayout(4, 2));
+		frame.setLayout(new BorderLayout());
+		frame.add(button, BorderLayout.LINE_END);
+		frame.add(new JButton("North"), BorderLayout.NORTH);
+		frame.add(new JButton("South"), BorderLayout.SOUTH);
+		frame.add(new JButton("East"), BorderLayout.EAST);
+		frame.add(new JButton("West"), BorderLayout.WEST);
+		frame.add(countryList, BorderLayout.CENTER);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
 
 }
