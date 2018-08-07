a41bfe2c3a684cd9af73b6190ab36c378a05b28c
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-08-07 18:42:29 +0200

    zadanie na wyjatki 1

--- src/main/java/pl/sdacademy/java11poz/hotel/Main.java
+++ src/main/java/pl/sdacademy/java11poz/hotel/Main.java
@@ -12,9 +12,15 @@ public class Main {
 		// pętli lub stream
 		// print
 		// i++ to jest to samo co i=i+1;
-		// tabliczkaMnozeniaPrzez3Wersja1();
+		tabliczkaMnozeniaPrzez3Wersja1();
 		tabliczkaMnozeniaPrzez3Wersja2();
-		// tabliczkaMnozeniaPrzez3Wersja3();
+		tabliczkaMnozeniaPrzez3Wersja3();
+		try {
+			tabliczkaMnozeniaWersja2(-71);
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 
 		// TODO zadanie 2
 		// zrob metode tabliczkaMnozeniaPrzez(int liczba)
@@ -24,6 +30,16 @@ public class Main {
 		// TODO zadanie 3
 		// pobieranie znaku z klawiary (konsoli)
 		// następnie wypisanie tabliczki mnożenia
+
+		// TODO zadanie 4 Wyjątki
+		// zrzuć wyjątek gdy napis jest nullem w metodzie dlugoscNapisu()
+		dlugoscNapisu("Ala ma kota");
+		String text = null;
+		dlugoscNapisu(text);
+	}
+
+	private static void dlugoscNapisu(String napis) {
+		System.out.println(0);
 	}
 
 	private static void tabliczkaMnozeniaPrzez3Wersja3() {
@@ -42,11 +58,20 @@ public class Main {
 		}
 	}
 
+	private static void tabliczkaMnozeniaWersja2(int liczba) {
+		if (liczba < 0) {
+			throw new IllegalArgumentException("Podaj liczbę wiekszą od 0");
+		}
+		for (int i = 1; i <= 10; i++) {
+			PrettyPrinter.wypiszTabliczke(i, liczba);
+		}
+	}
+
 	private static void tabliczkaMnozeniaPrzez3Wersja1() {
 		for (int i = 3; i < 100; i++) {
 			boolean mnozeniePrzezTrzy = i % 3 == 0;
 			if (mnozeniePrzezTrzy) {
-				PrettyPrinter.wypiszTabliczke(i, 3);
+				System.out.println(i);
 			}
 		}
 	}
